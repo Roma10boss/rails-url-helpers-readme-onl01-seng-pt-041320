c568b396551d4f4a10b5edea6935b0cea535503e
Romario Gustave <romagustave10@gmail.com>
2020-09-07 20:22:37 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -22,7 +22,7 @@ describe 'navigate' do
 end
 
 describe 'index page' do
-  it 'links to post page' do
+  #it 'links to post page' do
     second_post = Post.create(title: "My Title", description: "My post description")
     visit posts_path
     expect(page).to have_link(second_post.title, href: post_path(second_post))
